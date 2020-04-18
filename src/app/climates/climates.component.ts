9bbce48fe96d890c89506d0859b0e29206f7b9a9
burak.karaoglan <burakkaraoglan55@gmail.com>
2020-04-18 10:23:56 +0300

    style düzenleme ve icons

--- src/app/climates/climates.component.ts
+++ src/app/climates/climates.component.ts
@@ -1,11 +1,17 @@
-import {Component, OnDestroy, ViewChild} from '@angular/core';
-import { ClimateService } from 'src/app/services/climate.service';
-import {Observable, Subject, combineLatest, from, of} from 'rxjs';
-import {map, takeUntil} from "rxjs/operators";
-import {MatCheckboxChange} from "@angular/material/checkbox";
-import * as _ from 'lodash';
-import {animate, state, style, transition, trigger} from "@angular/animations";
-import { MatSort, MatTableDataSource} from "@angular/material";
+import { Component, OnDestroy, ViewChild } from "@angular/core";
+import { ClimateService } from "src/app/services/climate.service";
+import { Observable, Subject, combineLatest, from, of } from "rxjs";
+import { map, takeUntil } from "rxjs/operators";
+import { MatCheckboxChange } from "@angular/material/checkbox";
+import * as _ from "lodash";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { MatSort, MatTableDataSource } from "@angular/material";
 
 export interface Climate {
   id?: number;
@@ -13,6 +19,7 @@ export interface Climate {
   bookName: string;
   date: string;
   originalDate: string;
+  rangeEndDate?: string;
   dayExist?: boolean;
   monthExist?: boolean;
   pageNumber: string;
@@ -24,40 +31,57 @@ export interface Climate {
 }
 
 @Component({
-  selector: 'app-climates',
-  templateUrl: './climates.component.html',
-  styleUrls: ['./climates.component.css'],
+  selector: "app-climates",
+  templateUrl: "./climates.component.html",
+  styleUrls: ["./climates.component.css"],
   animations: [
-    trigger('detailExpand', [
-      state('void', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
-      state('*', style({ height: '*', visibility: 'visible' })),
-      transition('void <=> *', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    trigger("detailExpand", [
+      state(
+        "void",
+        style({ height: "0px", minHeight: "0", visibility: "hidden" })
+      ),
+      state("*", style({ height: "*", visibility: "visible" })),
+      transition("void <=> *", animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")),
     ]),
   ],
 })
 export class ClimatesComponent implements OnDestroy {
-  title = 'IKLIM PROJESI';
+  title = "IKLIM PROJESI";
 
   public _endSubscriptions$: Subject<boolean> = new Subject();
 
-  columnsToDisplay: string[] = ['#', 'text', 'place', 'date', 'originalDate', 'pageNumber', 'bookName', 'author', 'publisher', 'year', 'month', 'day', 'weatherTags'];
+  columnsToDisplay: string[] = [
+    "#",
+    "text",
+    "place",
+    "date",
+    "originalDate",
+    "bookName",
+    "author",
+    "publisher",
+    "year",
+    "month",
+    "day",
+    "weatherTags",
+  ];
 
   dataSource: MatTableDataSource<Climate>;
 
-  searchText: string = '';
+  searchText = "";
 
   dataFromService: Climate[] = [];
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   /**
    * Observable of the account state.
    *
    */
-  public climates$: Observable<Climate[]> = this.climateService.getClimates()
-    .pipe(
-      takeUntil(this._endSubscriptions$)
-    );
+  public climates$: Observable<
+    Climate[]
+  > = this.climateService
+    .getClimates()
+    .pipe(takeUntil(this._endSubscriptions$));
 
   yearCheck: boolean = undefined;
   monthCheck: boolean = undefined;
@@ -67,54 +91,58 @@ export class ClimatesComponent implements OnDestroy {
   public monthFilter$: Observable<Climate[]>;
   public dayFilter$: Observable<Climate[]>;
 
-
   hicriMonthMap: Map<string, string> = new Map<string, string>();
-
-  public fillHicriMonthMap(): void {
-    this.hicriMonthMap.set('M', 'MUHARREM');
-    this.hicriMonthMap.set('S', 'SAFER');
-    this.hicriMonthMap.set('RA', 'RABIULEVVEL');
-    this.hicriMonthMap.set('R', 'RABIULAHIR');
-    this.hicriMonthMap.set('CA', 'CEMAZIYELEVVEL');
-    this.hicriMonthMap.set('C', 'CEMAZIYELAHIR');
-    this.hicriMonthMap.set('B', 'RECEP');
-    this.hicriMonthMap.set('Ş', 'ŞABAN');
-    this.hicriMonthMap.set('N', 'RAMAZAN');
-    this.hicriMonthMap.set('L', 'SEVVAL');
-    this.hicriMonthMap.set('ZA', 'ZILKADE');
-    this.hicriMonthMap.set('Z', 'ZILHICCE');
-  }
-
-  //wi wi-day-sunny
 
   weatherTagsMap: Map<string, string> = new Map<string, string>();
 
-  public fillWeatherTagMap(): void {
-    this.weatherTagsMap.set('rüzgar', 'wi wi-windy');
-    this.weatherTagsMap.set('yağmur', 'wi wi-rain');
-    this.weatherTagsMap.set('yangın', 'wi wi-fire');
-    this.weatherTagsMap.set('veba', 'wi wi-alien'); //todo icon bul
-    this.weatherTagsMap.set('güneş', 'wi wi-day-sunny');
+  public fillHicriMonthMap(): void {
+    this.hicriMonthMap.set("M", "MUHARREM");
+    this.hicriMonthMap.set("S", "SAFER");
+    this.hicriMonthMap.set("RA", "RABIULEVVEL");
+    this.hicriMonthMap.set("R", "RABIULAHIR");
+    this.hicriMonthMap.set("CA", "CEMAZIYELEVVEL");
+    this.hicriMonthMap.set("C", "CEMAZIYELAHIR");
+    this.hicriMonthMap.set("B", "RECEP");
+    this.hicriMonthMap.set("Ş", "ŞABAN");
+    this.hicriMonthMap.set("N", "RAMAZAN");
+    this.hicriMonthMap.set("L", "SEVVAL");
+    this.hicriMonthMap.set("ZA", "ZILKADE");
+    this.hicriMonthMap.set("Z", "ZILHICCE");
   }
 
-  /**
-   *
-   MUHARREM("1", "M"),
-   SAFER("2", "S"),
-   RABIULEVVEL("3", "RA"),
-   RABIULAHIR("4", "R"),
-   CEMAZIYELEVVEL("5", "CA"),
-   CEMAZIYELAHIR("6", "C"),
-   RECEP("7", "B"),
-   SABAN("8", "Ş"),
-   RAMAZAN("9", "N"),
-   SEVVAL("10", "L"),
-   ZILKADE("11", "ZA"),
-   ZILHICCE("12", "Z");
+  public fillWeatherTagMap(): void {
+    this.weatherTagsMap.set("rüzgar", "wi wi-windy");
+    this.weatherTagsMap.set("yağmur", "wi wi-rain");
+    this.weatherTagsMap.set("yangın", "wi wi-fire");
+    this.weatherTagsMap.set("güneş", "wi wi-day-sunny");
+    this.weatherTagsMap.set("veba", "wi wi-alien"); //todo icon bul
+    this.weatherTagsMap.set("kar", "wi wi-snow");
+    this.weatherTagsMap.set("Şiddetli rüzgar", "wi wi-strong-wind");
+    this.weatherTagsMap.set("hafif rüzgar", "wi wi-rain-wind");
+    this.weatherTagsMap.set("sis", "wi wi-day-fog");
+    this.weatherTagsMap.set("şiddetli soğuk", "wi wi-snowflake-cold");
+    this.weatherTagsMap.set("yoğun kar", "wi wi-day-snow-thunderstorm");
+    this.weatherTagsMap.set("fırtına", "wi wi-hurricane");
+    this.weatherTagsMap.set("sel", "wi wi-flood");
+  }
+
+  /*
+   MUHARREM('1', 'M'),
+   SAFER('2', 'S'),
+   RABIULEVVEL('3', 'RA'),
+   RABIULAHIR('4', 'R'),
+   CEMAZIYELEVVEL('5', 'CA'),
+   CEMAZIYELAHIR('6', 'C'),
+   RECEP('7', 'B'),
+   SABAN('8', 'Ş'),
+   RAMAZAN('9', 'N'),
+   SEVVAL('10', 'L'),
+   ZILKADE('11', 'ZA'),
+   ZILHICCE('12', 'Z');
    */
 
   constructor(private climateService: ClimateService) {
-    this.climates$.subscribe(data => {
+    this.climates$.subscribe((data) => {
       this.dataSource = new MatTableDataSource<Climate>(data);
       this.dataFromService = data;
 
@@ -129,42 +157,61 @@ export class ClimatesComponent implements OnDestroy {
     });
   }
 
-  public getFullHicriMonthName(originalDate: string): string {
-    let shortHicriMonth: string = originalDate.split('-')[1];
-    return this.hicriMonthMap.has(shortHicriMonth) ? this.hicriMonthMap.get(shortHicriMonth) : null;
+  getFullHicriMonthName(originalDate: string): string {
+    const shortHicriMonth: string = originalDate.split("-")[1];
+    return this.hicriMonthMap.has(shortHicriMonth)
+      ? this.hicriMonthMap.get(shortHicriMonth)
+      : null;
   }
 
-  public getWeatherTagClass(tag: string): string {
-    return this.weatherTagsMap.get(tag);
+  getWeatherTagClass(tag: string): string {
+    if (!tag) return "";
+    const iconName = this.weatherTagsMap.get(tag);
+    return iconName ? iconName : "fa fa-window-close fa-x";
   }
 
-  public addSort(): void {
+  addSort(): void {
     this.dataSource.sort = this.sort;
   }
 
-  public ngOnDestroy(): void {
+  ngOnDestroy(): void {
     this._endSubscriptions$.next(true);
     this._endSubscriptions$.complete();
   }
 
   dateClicked($event: MatCheckboxChange) {
-
     if (this.yearCheck !== undefined) {
-      this.yearFilter$ = of(this.dataFromService).pipe(map(item => item.filter(climate => climate.yearExist === this.yearCheck)));
+      this.yearFilter$ = of(this.dataFromService).pipe(
+        map((item) =>
+          item.filter((climate) => climate.yearExist === this.yearCheck)
+        )
+      );
     }
 
     if (this.monthCheck !== undefined) {
-      this.monthFilter$ = of(this.dataFromService).pipe(map(item => item.filter(climate => climate.monthExist === this.monthCheck)));
+      this.monthFilter$ = of(this.dataFromService).pipe(
+        map((item) =>
+          item.filter((climate) => climate.monthExist === this.monthCheck)
+        )
+      );
     }
 
     if (this.dayCheck !== undefined) {
-      this.dayFilter$ = of(this.dataFromService).pipe(map(item => item.filter(climate => climate.dayExist === this.dayCheck)));
+      this.dayFilter$ = of(this.dataFromService).pipe(
+        map((item) =>
+          item.filter((climate) => climate.dayExist === this.dayCheck)
+        )
+      );
     }
 
-    combineLatest(this.yearFilter$, this.monthFilter$, this.dayFilter$, (year, month, day) => {
-      debugger
-      return _.intersectionWith(year, month, day, _.isEqual)
-    }).subscribe(data => {
+    combineLatest(
+      this.yearFilter$,
+      this.monthFilter$,
+      this.dayFilter$,
+      (year, month, day) => {
+        return _.intersectionWith(year, month, day, _.isEqual);
+      }
+    ).subscribe((data) => {
       this.dataSource.data = data;
     });
   }
@@ -179,7 +226,7 @@ export class ClimatesComponent implements OnDestroy {
     this.monthFilter$ = of(this.dataFromService);
     this.dayFilter$ = of(this.dataFromService);
 
-    this.searchText = '';
+    this.searchText = "";
     this.applyFilter(this.searchText);
   }
 
@@ -190,32 +237,36 @@ export class ClimatesComponent implements OnDestroy {
   }
 
   showSameDates() {
+    const valueArr = this.dataFromService.map((item) => {
+      return item.date;
+    });
 
-    var valueArr = this.dataFromService.map(function(item){ return item.date });
-
-    let duplicate = valueArr.reduce((acc,currentValue,index, array) => {
-      if(array.indexOf(currentValue)!=index || (array.indexOf(currentValue)==index && array.indexOf(currentValue, array.indexOf(currentValue) + 1) !== -1 )) {
+    const duplicate = valueArr.reduce((acc, currentValue, index, array) => {
+      if (
+        array.indexOf(currentValue) != index ||
+        (array.indexOf(currentValue) == index &&
+          array.indexOf(currentValue, array.indexOf(currentValue) + 1) !== -1)
+      ) {
         acc.push(this.dataFromService[index]);
       }
       return acc;
     }, []);
     this.dataSource.data = duplicate;
   }
-
 }
 
 /*
-author: "Federico Gravina"
-bookName: "İstanbul’ un Anlatımı"
-date: "1788-05-08"
+author: 'Federico Gravina'
+bookName: 'İstanbul’ un Anlatımı'
+date: '1788-05-08'
 dayExist: true
 id: 1
 monthExist: true
-pageNumber: "26"
-place: "İstanbul"
-publishedBy: "YKY"
-publishedDate: "2008"
-text: "... ‘Veba hastalığının aşırı salgın halde değilse de başkentte sürdüğünü ... ‘"
+pageNumber: '26'
+place: 'İstanbul'
+publishedBy: 'YKY'
+publishedDate: '2008'
+text: '... ‘Veba hastalığının aşırı salgın halde değilse de başkentte sürdüğünü ... ‘'
 yearExist: true
 
  */
