57b84bedfff0060490223d3d588ce61112bdee64
burak.karaoglan <burakkaraoglan55@gmail.com>
2021-02-24 17:47:36 +0300

    add image index

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,26 +3,38 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-home",
   template: `
-    <mat-card class="card"
-      ><a mat-raised-button href="mailto:iletisim@disar.org.tr"
-        >İLETİŞİM</a
-      ></mat-card
-    >
+    <div class="main-div">
+      <div class="center-div">
+        <mat-card class="card">
+          <a mat-raised-button href="mailto:iletisim@disar.org.tr">İLETİŞİM</a>
+          <img mat-card-image src="assets/onsozdusuk.jpg" alt="onsoz" />
+        </mat-card>
+      </div>
+    </div>
   `,
   styles: [
     `
       a {
-        padding-left: 20px;
-        padding-top: 20px;
-        align: center;
+        position: absolute;
+        z-index: 99;
+      }
+
+      .center-div {
+        position: absolute;
+      }
+
+      .main-div {
+        display: flex;
+
+        justify-content: center;
       }
 
       .card {
-        background-image: url(assets/onsozdusuk.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center center;
-        height: 1000px;
+        max-width: 1050px;
+      }
+
+      mat-card img {
+        object-fit: cover; /*this makes the image in src fit to the size specified below*/
       }
     `,
   ],
